0bb94829fadab7b93f7f7e63570d6d62b61e237a
yonaCluster <yeonhkim@student.42seoul.kr>
2024-01-19 18:33:16 +0900

    FY-61/feat: 리액트 앱 개발 초기세팅
    - 모듈 번들러 Webpack, 트랜스파일러 Babel 설치 및 세팅
    - 메인 HTML 템플릿 /public/index.html 추가
    - 메인 JS 파일 /src/index.js 추가
    - 최상단 컴포넌트 App 추가

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+    mode: "development",
+    entry: "./src/index.js",
+    output: {
+        filename: "bundle.js",
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                },
+            },
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"],
+            },
+        ],
+    },
+    resolve: {
+        extensions: [".js", ".jsx"],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./public/index.html",
+        }),
+    ],
+    devServer: {
+        port: 4242,
+        static: {
+            directory: path.join(__dirname, "dist"),
+        },
+        hot: true,
+        open: true,
+    },
+    stats: "minimal", // 출력되는 로그 메세지는 최소한으로 (차후 'none'으로 변경 고려)
+};
