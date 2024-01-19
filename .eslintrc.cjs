2e4a91932c2d39ab9920fd1e26a7e24a073b433d
JinMu Go <jingmuio@gmail.com>
2024-01-19 16:24:34 +0900

    FY-74/chore: init frontend env setting
    RFS project와 동일하게 환경을 구성하였습니다.

---
+++ .eslintrc.cjs
@@ -0,0 +1,40 @@
+"use strict";
+
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+    },
+    extends: ["prettier", "plugin:prettier/recommended"],
+    plugins: ["prettier"],
+    overrides: [
+        {
+            env: {
+                node: true,
+            },
+            files: [".eslintrc.{js,cjs}"],
+            parserOptions: {
+                sourceType: "script",
+            },
+        },
+    ],
+    parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+    },
+    rules: {
+        strict: ["error", "global"], // "use strict"
+        camelcase: "error", // camelCase 사용
+        "no-var": "error", // var is not allowed
+        indent: ["error", 4], // 4 spaces
+        "linebreak-style": ["error", "unix"], // LF
+        quotes: ["error", "double"], // use double quotes
+        semi: ["error", "always"], // always use semicolon
+        eqeqeq: ["error", "always"], // always use ===
+        "prettier/prettier": "error", // Prettier
+        "prefer-const": "error", // use const
+        "func-style": ["error", "expression"], // 함수 표현식 스타일 권장
+        "prefer-rest-params": "error", // Rest 파라미터 사용 권장
+        "prefer-spread": "error", // spread 연산자 사용 권장
+    },
+};
