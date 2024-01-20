c2c7ca19547c925c3777ef6d01ab156e4003d597
yonaCluster <yeonhkim@student.42seoul.kr>
2024-01-20 13:15:51 +0900

    FY-75/feat: 최상단 컴포넌트에 라우팅 및 테스트용 네비게이션 추가

--- src/App.js
+++ src/App.js
@@ -1,11 +1,44 @@
-import React from "react";
+import React, { createContext, useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import HomePage from "./pages/HomePage";
+import GamePage from "./pages/GamePage";
+import FriendsPage from "./pages/FriendsPage";
+import LoginPage from "./pages/LoginPage";
+import ProfilePage from "./pages/ProfilePage";
+import RoomListPage from "./pages/RoomListPage";
+import ChatPage from "./pages/ChatPage";
+import UserPage from "./pages/UserPage";
+import NotFoundPage from "./pages/NotFoundPage";
+
+import Navi from "./Navi";
+
+export const GlobalContext = createContext(); // 정보가 많아질시 차후 별도 context 파일들로 분리
 
 const App = () => {
-    return (
+  const [someData, setSomeData] = useState();
+
+  return (
+    <Router>
+      <GlobalContext.Provider value={someData}>
         <div className="App">
-			Welcome to PongDom.
+          <Navi />
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/game" element={<GamePage />} />
+            <Route path="/friends" element={<FriendsPage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/profile" element={<ProfilePage />} />
+            <Route path="/roomlist" element={<RoomListPage />} />
+            <Route path="/chat" element={<ChatPage />} />
+            <Route path="/user" element={<UserPage />} />
+            {/* fallback page */}
+            <Route path="*" element={<NotFoundPage />} />
+          </Routes>
         </div>
-    );
+      </GlobalContext.Provider>
+    </Router>
+  );
 };
 
 export default App;
