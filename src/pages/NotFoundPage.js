bba09124b1cd113bb4dc997ddecc1f49d1a61f01
yonaCluster <yeonhkim@student.42seoul.kr>
2024-01-20 13:15:04 +0900

    FY-75/feat: NotFoundPage, UserPage 추가

---
+++ src/pages/NotFoundPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NotFoundPage = () => {
+  return (
+    <div className="NotFoundPage">
+      유효하지 않은 경로입니다. 홈으로 리다이렉트합니다...
+    </div>
+  );
+};
+
+export default NotFoundPage;
---
+++ src/pages/UserPage.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const UserPage = () => {
+  return <div className="UserPage">This is UserPage</div>;
+};
+
+export default UserPage;
