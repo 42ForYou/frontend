5b815a028cfa67fa0d2c8c6ace2abbc7a6f981e2
yonaCluster <yeonhkim@student.42seoul.kr>
2024-01-22 15:18:09 +0900

    FY-75/feat: 기본 레이아웃 컴포넌트(페이지 컨테이너, 컨텐츠 컨테이너,  네비게이션 바) 구성

--- src/components/MainContainer.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const MainContainer = () => {
-  return <div className="MainContainer"></div>;
-};
-
-export default MainContainer;
--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,7 +1,38 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const NavigationBar = () => {
-  return <div className="NavigationBar"></div>;
+  return (
+    <div className="NavigationBar">
+      <nav>
+        <div>LOGO</div>
+        <ul>
+          <li>
+            <Link to={"/"}>HOME</Link>
+          </li>
+          <li>
+            <Link to={"/games"}>GAMES</Link>
+          </li>
+          <li>
+            <Link to={"/chat"}>CHAT</Link>
+          </li>
+          <li>
+            <Link to={"/friends"}>FRIENDS</Link>
+          </li>
+          <li>
+            <Link to={"/users"}>USERS</Link>
+          </li>
+          <li>
+            {/* 알람은 페이지가 아님 차후 알맞게 수정 */}
+            <Link to={"/unknown"}>NOTIFICATION</Link>
+          </li>
+          <li>
+            <Link to={"/profile"}>PROFILE</Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
 };
 
 export default NavigationBar;
