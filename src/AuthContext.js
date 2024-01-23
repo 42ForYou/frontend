7dc9ae6a39e35cce879850f53887a80fc0d4051c
yonaCluster <yeonhkim@student.42seoul.kr>
2024-01-23 16:02:40 +0900

    FY-43/feat: 인증 컨텍스트 AuthContext 추가

--- src/AuthContext.js
+++ src/AuthContext.js
@@ -6,6 +6,7 @@ const AuthContext = createContext();
 // AuthProvider 컴포넌트
 export const AuthProvider = ({ children }) => {
   const [authToken, setAuthToken] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
 
   // 토큰 설정 함수
   const handleSetAuthToken = (token) => {
@@ -19,12 +20,28 @@ export const AuthProvider = ({ children }) => {
     setAuthToken(null);
   };
 
+  // todo: 토큰 유효성 검사 로직 구현
+  const isValidToken = (token) => {
+    return token === "dev-token";
+  };
+
   return (
     <AuthContext.Provider
-      value={{ authToken, setAuthToken: handleSetAuthToken, removeAuthToken: handleRemoveAuthToken }}>
+      value={{
+        authToken,
+        setAuthToken: handleSetAuthToken,
+        removeAuthToken: handleRemoveAuthToken,
+        isValidToken,
+        isLoading,
+        setIsLoading,
+      }}>
       {children}
     </AuthContext.Provider>
   );
 };
+
+// AuthContext를 사용하기 위한 커스텀 훅
+// 차후 코드 간결화 시 적용 고려
+export const useAuth = () => useContext(AuthContext);
 
 export default AuthContext;
