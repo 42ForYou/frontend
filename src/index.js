5291f8579bbba6e0411c7c36584a05f19ea93eb7
yonaCluster <yeonhkim@student.42seoul.kr>
2024-01-23 16:15:35 +0900

    FY-43/chore: React.StrictMode 해제
    useEffect가 두 번 실행되지 않도록 임시 해제

--- src/index.js
+++ src/index.js
@@ -4,9 +4,10 @@ import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
+// 엄격 모드 적용시 부수 효과 감지를 위해 마운트, 언마운트가 두 번씩 실행됨
 // 프로덕션에서 React.StrictMode는 필요없음
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  // <React.StrictMode>
+  <App />
+  // </React.StrictMode>
 );
