154feb331a9caa9705e8942c86be8536d7ace7b6
yonaCluster <yeonhkim@student.42seoul.kr>
2024-01-25 15:40:43 +0900

    FY-99/style: 네비게이션 바에 부트스트랩 적용

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -2,6 +2,14 @@ import React, { useContext } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import AuthContext from "../AuthContext";
 
+const NavItem = ({ to, label }) => (
+  <li className="nav-item ms-3">
+    <Link to={to} className="nav-link">
+      {label}
+    </Link>
+  </li>
+);
+
 const NavigationBar = () => {
   const { removeAuthToken } = useContext(AuthContext);
   const navigate = useNavigate();
@@ -14,42 +22,24 @@ const NavigationBar = () => {
 
   return (
     <div className="NavigationBar border-p3-box">
-      <nav>
-        <div>LOGO</div>
-        <ul>
-          <li>
-            <Link to={"/"}>HOME</Link>
-          </li>
-          <li>
-            <Link to={"/games"}>GAMES</Link>
-          </li>
-          <li>
-            <Link to={"/chat"}>CHAT</Link>
-          </li>
-          <li>
-            <Link to={"/friends"}>FRIENDS</Link>
-          </li>
-          <li>
-            <Link to={"/users"}>USERS</Link>
-          </li>
-          <li>NOTIFICATION</li>
-          <li>
-            <Link to={"/profile"}>PROFILE (MY)</Link>
-          </li>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="navbar-brand mx-auto">LOGO</div>
+        <ul className="navbar-nav">
+          <NavItem to={"/"} label="HOME" />
+          <NavItem to={"/games"} label="GAMES" />
+          <NavItem to={"/chat"} label="CHAT" />
+          <NavItem to={"/friends"} label="FRIENDS" />
+          <NavItem to={"/users"} label="USERS" />
+          <li className="nav-item ms-3">NOTIFICATION</li>
+          <NavItem to={"/profile"} label="PROFILE (MY)" />
         </ul>
-        <button onClick={handleLogout}>로그아웃하기</button>
-        <div>
-          <br />
-          ---- 프로필 테스트용 ----
-          <li>
-            <Link to={"/profile/users/pikachu"}>PROFILE (Valid 1)</Link>
-          </li>
-          <li>
-            <Link to={"/profile/users/jgo"}>PROFILE (Valid 2)</Link>
-          </li>
-          <li>
-            <Link to={"/profile/users/someone"}>PROFILE (Invalid)</Link>
-          </li>
+        <div className="mt-2 mx-auto text-center">
+          <button className="btn btn-primary" onClick={handleLogout}>
+            로그아웃하기
+          </button>
+          <button className="btn btn-secondary mt-2" onClick={() => navigate("/test")}>
+            테스트용 페이지로
+          </button>
         </div>
       </nav>
     </div>
