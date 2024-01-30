3ae9ba2a72a12b2c118b8d07f125e0a36b284988
yonaMac <yonaimyo@gmail.com>
2024-01-30 20:32:39 +0900

    FY-108/fix: 쿠키에서 토큰을 삭제한 뒤에도 보호된 페이지에 접근 가능한 현상 고침

--- src/withAuthProtection.js
+++ src/withAuthProtection.js
@@ -11,7 +11,13 @@ const withAuthProtection = (WrappedComponent) => {
     const navigate = useNavigate();
     const location = useLocation();
 
+    // 보호된 페이지로 접근 시도할 때마다 인증 검사
+    // 1차 확인: context의 토큰
+    // 2차 확인: 쿠키에 저장되어 있는 토큰
     useEffect(() => {
+      setToken(false);
+      setIsAuthorized(false);
+
       const authCheck = async () => {
         const isStateTokenValid = await validateToken(token);
         // console.log("token(state): ", isStateTokenValid);
@@ -26,13 +32,13 @@ const withAuthProtection = (WrappedComponent) => {
             setToken(tokenFromCookie);
             setIsAuthorized(true);
           } else {
-            navigate("/login");
             alert("토큰이 유효하지 않습니다. 로그인 페이지로 돌아갑니다");
+            navigate("/login");
           }
         }
       };
       authCheck();
-    }, [token, location]);
+    }, [location]);
 
     return isAuthorized ? <WrappedComponent {...props} /> : null;
   };
