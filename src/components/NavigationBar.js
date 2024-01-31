cbca7b8ebbbea00fcaddd9fd3a26387051c042af
yonaMac <yonaimyo@gmail.com>
2024-01-31 19:41:54 +0900

    FY-108/refactor: 네비게이션 바 코드 분리

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -13,16 +13,53 @@ const NavItem = ({ icon, label, to }) => (
   </li>
 );
 
-const NavigationBar = () => {
+const NavItems = () => {
+  return (
+    <ul>
+      <NavItem to={"/"} label="HOME" icon={"https://cdn0.iconfinder.com/data/icons/typicons-2/24/home-48.png"} />
+      <NavItem
+        to={"/games"}
+        label="GAMES"
+        icon={"https://cdn3.iconfinder.com/data/icons/remixicon-others/24/ping-pong-fill-48.png"}
+      />
+      <NavItem
+        to={"/friends"}
+        label="FRIENDS"
+        icon={"https://cdn4.iconfinder.com/data/icons/ionicons/512/icon-ios7-heart-512.png"}
+      />
+      <NavItem
+        to={"/users"}
+        label="USERS"
+        icon={"https://cdn4.iconfinder.com/data/icons/pictype-free-vector-icons/16/group-48.png"}
+      />
+      <NavItem label="NOTICE" icon={"https://cdn4.iconfinder.com/data/icons/ionicons/512/icon-ios7-bell-48.png"} />
+      <NavItem to={"/test"} label="테스트용" />
+    </ul>
+  );
+};
+
+const NavUser = () => {
   const { userProfile, logout } = useContext(AuthContext);
   const navigate = useNavigate();
 
   const handleLogout = () => {
     logout();
-    alert("로그아웃 되었습니다. 로그인 페이지로 돌아갑니다");
+    alert("로그아웃 되었습니다. (httpOnly 쿠키는 삭제되지 않음)\n로그인 페이지로 돌아갑니다");
     navigate("/login");
   };
 
+  return (
+    <div className="text-center d-flex flex-column">
+      {userProfile ? userProfile.nickname : "로그인하지 않은 사용자"}
+      <Avatar to={"/profile"} />
+      <button className="btn btn-primary mt-2 w-40" onClick={handleLogout}>
+        로그아웃하기
+      </button>
+    </div>
+  );
+};
+
+const NavigationBar = () => {
   return (
     <div className="NavigationBar border-p3-box h-100">
       <nav className="bg-light p-3 d-flex flex-column justify-content-between h-100">
@@ -31,47 +68,12 @@ const NavigationBar = () => {
             <div>LOGO</div>
           </div>
           <div className="row">
-            <ul>
-              <NavItem
-                to={"/"}
-                label="HOME"
-                icon={"https://cdn0.iconfinder.com/data/icons/typicons-2/24/home-48.png"}
-              />
-              <NavItem
-                to={"/games"}
-                label="GAMES"
-                icon={"https://cdn3.iconfinder.com/data/icons/remixicon-others/24/ping-pong-fill-48.png"}
-              />
-              <NavItem
-                to={"/friends"}
-                label="FRIENDS"
-                icon={"https://cdn4.iconfinder.com/data/icons/ionicons/512/icon-ios7-heart-512.png"}
-              />
-              <NavItem
-                to={"/users"}
-                label="USERS"
-                icon={"https://cdn4.iconfinder.com/data/icons/pictype-free-vector-icons/16/group-48.png"}
-              />
-              <NavItem
-                label="NOTICE"
-                icon={"https://cdn4.iconfinder.com/data/icons/ionicons/512/icon-ios7-bell-48.png"}
-              />
-              <NavItem to={"/test"} label="테스트용" />
-            </ul>
+            <NavItems />
           </div>
         </div>
-        <div className="row text-center d-flex flex-column">
-          {userProfile ? userProfile.nickname : "로그인하지 않은 사용자"}
-          <Avatar to={"/profile"} />
-          <button className="btn btn-primary mt-2 w-40" onClick={handleLogout}>
-            로그아웃하기
-          </button>
+        <div className="row">
+          <NavUser />
         </div>
-        {/* <div className="mt-2 mx-auto text-center d-flex flex-column">
-          <button className="btn btn-secondary mt-2" onClick={() => navigate("/test")}>
-            테스트용
-          </button>
-        </div> */}
       </nav>
     </div>
   );
