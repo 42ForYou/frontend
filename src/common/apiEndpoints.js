78dcf7e2fce2ac2f275cf84069a41cf1ed8cd623
yonaMac <yonaimyo@gmail.com>
2024-02-01 06:46:50 +0900

    FY-114/refactor: 필요없는 주석 삭제

--- src/common/apiEndpoints.js
+++ src/common/apiEndpoints.js
@@ -6,7 +6,3 @@ export const API_ENDPOINTS = {
   ROOM_LIST: (is_tournament, page, page_size) =>
     `/game/game_rooms/?is_tournament=${is_tournament}&page=${page}&page_size=${page_size}`,
 };
-
-// 사용 예시
-// import { API_ENDPOINTS } from './apiEndpoints';
-// axios.get(API_ENDPOINTS.USER_PROFILE);
