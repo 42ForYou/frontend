e3886acca01d6e0bc4cf5abb5f46d2abb0fe254e
yonaMac <yonaimyo@gmail.com>
2024-02-01 05:31:26 +0900

    FY-114/feat: 엔드포인트 상수에 OAUTH_REDIRECT 추가

--- src/common/apiEndpoints.js
+++ src/common/apiEndpoints.js
@@ -1,5 +1,6 @@
 export const API_ENDPOINTS = {
   LOGIN: "/login",
+  OAUTH_REDIRECT: (code) => `/oauth/?code=${code}`,
   VALID: "/valid",
   USER_PROFILE: (nickname) => `/accounts/profiles/${nickname}`,
   ROOM_LIST: (is_tournament, page, page_size) =>
