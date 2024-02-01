af18aacbfe9f883266ba273e888e10e9f48d6fa2
yonaMac <yonaimyo@gmail.com>
2024-02-01 19:56:14 +0900

    FY-118/chore: 엔드포인트 USER_PROFILE 조건부 값 결정

--- src/common/apiEndpoints.js
+++ src/common/apiEndpoints.js
@@ -2,7 +2,9 @@ export const API_ENDPOINTS = {
   LOGIN: "/login",
   OAUTH_REDIRECT: (code) => `/oauth/?code=${code}`,
   VALID: "/valid",
-  USER_PROFILE: (intra_id) => `/accounts/profiles/${intra_id}`,
+  USER_PROFILE: (intra_id) => {
+    intra_id ? `/accounts/profiles/${intra_id}/` : "/accounts/profiles/";
+  },
   ROOM_LIST: (is_tournament, page, page_size) =>
     `/game/game_rooms/?is_tournament=${is_tournament}&page=${page}&page_size=${page_size}`,
 };
