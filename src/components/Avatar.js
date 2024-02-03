a67e492d10541d7c384fd6ee3bb45b54f4a312bd
yonaMac <yonaimyo@gmail.com>
2024-02-03 12:15:49 +0900

    FY-118/refactor: 아바타 컴포넌트 중복 코드 리팩토링

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -1,41 +1,23 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const Avatar = ({ src, alt, to }) => {
-  const defaultImage =
-    "https://static.vecteezy.com/system/resources/thumbnails/009/292/244/small/default-avatar-icon-of-social-media-user-vector.jpg";
+const defaultImage =
+  "https://static.vecteezy.com/system/resources/thumbnails/009/292/244/small/default-avatar-icon-of-social-media-user-vector.jpg";
 
-  return (
-    <div className="Avatar">
-      {to ? (
-        <Link to={to}>
-          <img
-            src={src || defaultImage}
-            alt={alt || "프로필 이미지"}
-            style={{
-              width: "100px",
-              height: "100px",
-              borderRadius: "50%",
-              objectFit: "cover",
-              border: "2px solid #f000f0",
-            }}
-          />
-        </Link>
-      ) : (
-        <img
-          src={src || defaultImage}
-          alt={alt || "프로필 이미지"}
-          style={{
-            width: "100px",
-            height: "100px",
-            borderRadius: "50%",
-            objectFit: "cover",
-            border: "2px solid #f000f0",
-          }}
-        />
-      )}
-    </div>
-  );
+const imageSizeStyle = {
+  width: "100px",
+  height: "100px",
+  borderRadius: "50%",
+  objectFit: "cover",
+  border: "2px solid #f000f0",
+};
+
+const Avatar = ({ src, alt, to }) => {
+  const imageSrc = src === "default.jpg" ? defaultImage : src || defaultImage;
+  const imageAlt = alt || "프로필 이미지";
+  const imageElement = <img src={imageSrc} alt={imageAlt} style={imageSizeStyle} />;
+
+  return <div className="Avatar">{to ? <Link to={to}>{imageElement}</Link> : imageElement}</div>;
 };
 
 export default Avatar;
