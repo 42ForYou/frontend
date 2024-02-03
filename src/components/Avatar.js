1d4eb4a53ee75fca2f0d66016d5d7a0c36ed920f
yonaMac <yonaimyo@gmail.com>
2024-02-03 12:21:19 +0900

    FY-118/style: 아바타 컴포넌트 이미지 로드 실패시 디폴트 이미지 띄움

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -1,9 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const defaultImage =
-  "https://static.vecteezy.com/system/resources/thumbnails/009/292/244/small/default-avatar-icon-of-social-media-user-vector.jpg";
-
 const imageSizeStyle = {
   width: "100px",
   height: "100px",
@@ -13,9 +10,15 @@ const imageSizeStyle = {
 };
 
 const Avatar = ({ src, alt, to }) => {
-  const imageSrc = src === "default.jpg" ? defaultImage : src || defaultImage;
+  const defaultImage =
+    "https://static.vecteezy.com/system/resources/thumbnails/009/292/244/small/default-avatar-icon-of-social-media-user-vector.jpg";
+  const imageSrc = src || defaultImage;
   const imageAlt = alt || "프로필 이미지";
-  const imageElement = <img src={imageSrc} alt={imageAlt} style={imageSizeStyle} />;
+
+  const handleImageLoadFailed = (e) => {
+    e.target.src = defaultImage;
+  };
+  const imageElement = <img src={imageSrc} alt={imageAlt} style={imageSizeStyle} onError={handleImageLoadFailed} />;
 
   return <div className="Avatar">{to ? <Link to={to}>{imageElement}</Link> : imageElement}</div>;
 };
