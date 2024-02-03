e3c0bd3bb7db467460c81fead6eb88d7c21a7345
yonaMac <yonaimyo@gmail.com>
2024-02-03 13:57:25 +0900

    FY-118/style: 아바타 컴포넌트 수정 가능 스타일링 추가

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -1,15 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const imageSizeStyle = {
-  width: "100px",
-  height: "100px",
-  borderRadius: "50%",
-  objectFit: "cover",
-  border: "2px solid #f000f0",
-};
-
-const Avatar = ({ src, alt, to }) => {
+const Avatar = ({ src, alt, to, isEditable = false }) => {
   const defaultImage =
     "https://static.vecteezy.com/system/resources/thumbnails/009/292/244/small/default-avatar-icon-of-social-media-user-vector.jpg";
   const imageSrc = src || defaultImage;
@@ -18,9 +10,27 @@ const Avatar = ({ src, alt, to }) => {
   const handleImageLoadFailed = (e) => {
     e.target.src = defaultImage;
   };
-  const imageElement = <img src={imageSrc} alt={imageAlt} style={imageSizeStyle} onError={handleImageLoadFailed} />;
 
-  return <div className="Avatar">{to ? <Link to={to}>{imageElement}</Link> : imageElement}</div>;
+  const renderImage = () => (
+    <img
+      src={imageSrc}
+      alt={imageAlt}
+      className="rounded-circle" // 부트스트랩 클래스
+      style={{ objectFit: "cover", border: "2px solid #f000f0", width: "100px" }}
+      onError={handleImageLoadFailed}
+    />
+  );
+
+  return (
+    <div className="Avatar position-relative" style={{ width: "100px", height: "100px" }}>
+      {to ? <Link to={to}>{renderImage()}</Link> : renderImage()}
+      {isEditable && (
+        <i
+          className="fas fa-edit position-absolute"
+          style={{ top: "0", right: "0", fontSize: "24px", color: "#000", cursor: "pointer" }}></i>
+      )}
+    </div>
+  );
 };
 
 export default Avatar;
