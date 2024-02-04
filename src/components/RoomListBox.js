30fb7e349f12cc3be671697b3ac8c657dea3e08d
yonaMac <yonaimyo@gmail.com>
2024-02-04 17:16:58 +0900

    FY-105/feat: RoomListBox  방 생성 버튼 추가

--- src/components/RoomListBox.js
+++ src/components/RoomListBox.js
@@ -2,16 +2,27 @@ import React, { useState, useEffect } from "react";
 
 import RoomItem from "./RoomItem";
 import ListBox from "./ListBox";
-import { API_ENDPOINTS } from "../common/apiEndPoints";
+import { API_ENDPOINTS } from "../common/apiEndpoints";
+import CreateRoomModal from "./CreateRoomModal";
 
 const RoomListBox = () => {
+  const [showModal, setShowModal] = useState(false);
+  const handleCreateRoomClick = () => setShowModal(true);
+  const handleCloseModal = () => setShowModal(false);
+
   return (
     <>
       <ListBox
         apiEndpoint={API_ENDPOINTS.ROOM_LIST}
         ItemComponent={RoomItem}
         filterTypes={["All", "1vs1", "Tournament"]}
+        additionalButton={
+          <button className="btn btn-primary" onClick={handleCreateRoomClick}>
+            방 생성
+          </button>
+        }
       />
+      {showModal && <CreateRoomModal handleClose={handleCloseModal} />}
     </>
   );
 };
