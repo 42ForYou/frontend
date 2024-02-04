ce909ddec42cd51a03ddf603942f4e3758d95361
yonaMac <yonaimyo@gmail.com>
2024-02-05 06:13:06 +0900

    FY-105/fix: 룸 아이템 컴포넌트 props 받아오는 코드 고침

--- src/components/RoomItem.js
+++ src/components/RoomItem.js
@@ -2,7 +2,10 @@ import React from "react";
 
 // 스타일을 가지는 박스
 // 일단은 1대1도 토너먼트 스타일과 통일
-const RoomItem = ({ title, host, nPlayers, timeLimit, gamePoint, roomId }) => {
+const RoomItem = ({ game, room }) => {
+  const { game_id, is_tournament, game_point, time_limit, n_players } = game;
+  const { id, title, is_playing, join_players, host } = room;
+
   const handleJoinClick = (roomId) => {
     // 서버에 방 참가 요청
     console.log("방 참가 요청");
@@ -11,19 +14,21 @@ const RoomItem = ({ title, host, nPlayers, timeLimit, gamePoint, roomId }) => {
   return (
     <div className="RoomItem border border-primary w-100 p-3">
       <div className="row justify-space-between">
-        <div className="col">{title}</div>
-        <div className="col text-end">{nPlayers === 2 ? "[1vs1]" : `[${nPlayers / 2}강]`}</div>
+        <div className="col">
+          <h5>{title}</h5>
+        </div>
+        <div className="col text-end">{is_tournament === 2 ? "[1vs1]" : "[토너먼트]"}</div>
       </div>
       <div className="row">
         <div className="col-9">
           방장: {host}
           <br />
-          인원 수: {nPlayers}
+          인원 수: {join_players} / {n_players}
           <br />
-          승리 요건: 제한시간 {timeLimit}초, 득점 {gamePoint}점
+          승리 요건: 제한시간 {time_limit}초, 득점 {game_point}점
         </div>
         <div className="col container-fluid d-flex flex-column align-items-end">
-          <button onClick={() => handleJoinClick(roomId)}>JOIN</button>
+          <button onClick={() => handleJoinClick(game_id)}>JOIN</button>
         </div>
       </div>
     </div>
