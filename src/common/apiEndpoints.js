173ed3fcaf98ebdd4e2b9169d0847e9b527eef5a
yonaMac <yonaimyo@gmail.com>
2024-02-04 20:24:18 +0900

    FY-105/chore: 엔드포인트 변경 반영

--- src/common/apiEndpoints.js
+++ src/common/apiEndpoints.js
@@ -2,6 +2,9 @@ export const API_ENDPOINTS = {
   LOGIN: "/login",
   OAUTH_REDIRECT: (code) => `/oauth/?code=${code}`,
   VALID: "/valid",
-  USER_PROFILE: (nickname) => `/accounts/profiles/${nickname}`,
-  ROOM_LIST: (type, page, page_size) => `/game/game_rooms/?filter=${type}&page=${page}&page_size=${page_size}`,
+  USER_PROFILE: (nickname) => `api/accounts/profiles/${nickname}`,
+  ROOM_LIST: (type, page, page_size) => {
+    if (type && page && page_size) return `api/game/game_rooms/?filter=${type}&page=${page}&page_size=${page_size}`;
+    return "api/game/game_rooms/";
+  },
 };
