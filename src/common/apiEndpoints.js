6fedd6984ace74c34d7754fafd72267960f402e2
yonaMac <yonaimyo@gmail.com>
2024-02-05 18:24:30 +0900

    FY-105/chore: 룸 리스트 필터 값 all일시 API URL 수정

--- src/common/apiEndpoints.js
+++ src/common/apiEndpoints.js
@@ -4,7 +4,10 @@ export const API_ENDPOINTS = {
   VALID: "/valid",
   USER_PROFILE: (nickname) => `api/accounts/profiles/${nickname}`,
   ROOM_LIST: (type, page, page_size) => {
-    if (type && page && page_size) return `api/game/game_rooms/?filter=${type}&page=${page}&page_size=${page_size}`;
+    if (type && page && page_size) {
+      if (type === "all") return `api/game/game_rooms/?page=${page}&page_size=${page_size}`;
+      else return `api/game/game_rooms/?filter=${type}&page=${page}&page_size=${page_size}`;
+    }
     return "api/game/game_rooms/";
   },
 };
