5f0c10b83b4ef5a66ce797a7b1529d261d16e8f7
yonaMac <yonaimyo@gmail.com>
2024-02-06 17:24:19 +0900

    FY-123/chore: 에셋 정적 이미지 제공을 위한 nginx, 웹팩 설정 변경

--- webpack.common.js
+++ webpack.common.js
@@ -16,8 +16,8 @@ module.exports = {
     new DotenvWebpackPlugin(),
     new CopyWebpackPlugin({
       patterns: [
-        { from: "public/favicon.ico", to: "" }, // src 디렉토리에서 루트 출력 디렉토리로 favicon.ico 복사
-        { from: "public/images", to: "images" },
+        { from: "public/favicon.ico", to: "" },
+        { from: "public/assets", to: "assets" },
       ],
     }),
   ],
