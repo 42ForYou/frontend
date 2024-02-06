c8baed38ea2f03f2b908944a8029561ac31f430c
JinMu Go <jingmuio@gmail.com>
2024-02-06 16:31:21 +0900

    FY-109/fix(src): fix src typo
    이전 커밋에서 발생한 오류 typing을 수정하였습니다.

--- src/common/apiEndpoints.js
+++ src/common/apiEndpoints.js
@@ -2,7 +2,7 @@ export const API_ENDPOINTS = {
   LOGIN: "/login",
   OAUTH_REDIRECT: (code) => `/oauth/?code=${code}`,
   VALID: "/valid",
-  USER_PROFILE: (nickname) => `api/accounts/profiles/${nickname}`,
+  USER_PROFILE: (nickname) => `api/accounts/profiles/${nickname}/`,
   ROOM_LIST: (type, page, page_size) => {
     if (type && page && page_size) {
       if (type === "all") return `api/game/game_rooms/?page=${page}&page_size=${page_size}`;
