1afb94d272f5c8532bf24f9fe36aeb653ab14c9a
yonaMac <yonaimyo@gmail.com>
2024-02-07 02:19:12 +0900

    FY-95/feat: StyledButton 컴포넌트에 버튼 비활성화 여부 props 추가

--- src/components/StyledButton.js
+++ src/components/StyledButton.js
@@ -1,9 +1,14 @@
 import React from "react";
 
-const StyledButton = ({ styleType, name, onClick, overrideStyle }) => {
+const StyledButton = ({ styleType, name, onClick, overrideStyle, disabled }) => {
   const btnClass = `btn btn-${styleType}`;
+  const buttonStyle = {
+    ...overrideStyle,
+    opacity: disabled ? 0.5 : 1, // 비활성화 상태일 때 투명도를 조절하여 비활성화 효과를 줌
+  };
+
   return (
-    <button type="button" className={btnClass} onClick={onClick} style={overrideStyle}>
+    <button type="button" className={btnClass} onClick={onClick} style={buttonStyle} disabled={disabled}>
       {name}
     </button>
   );
