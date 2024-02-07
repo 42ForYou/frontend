deb5ec1bb33d057bd2d5ace296ecf79f56ae44e5
yonaMac <yonaimyo@gmail.com>
2024-02-07 19:46:33 +0900

    FY-125/feat: 아바타 컴포넌트 전달된 src 값에 따라 리렌더링

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -1,22 +1,26 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 const Avatar = ({ src, alt, to, isEditing = false, onImageUploadClick, diameter = 100 }) => {
+  const [imageSrc, setImageSrc] = useState(src);
+
+  useEffect(() => {
+    setImageSrc(src);
+  }, [src]);
+
   const getImageSrc = (src) => {
     if (!src || src === "default.jpg") return `${process.env.ASSETS_URL}/images/default-avatar.jpg`;
     return `${process.env.API_BASE_URL}/images/avatar/${src}`;
   };
-  const [imageSrc, setImageSrc] = useState(getImageSrc(src));
-  const imageAlt = alt || "아바타 이미지";
 
-  const handleImageLoadFailed = (e) => {
+  const handleImageLoadFailed = () => {
     setImageSrc(`${process.env.ASSETS_URL}/images/loading-failed.png`);
   };
 
   const renderImage = () => (
     <img
-      src={imageSrc}
-      alt={imageAlt}
+      src={getImageSrc(imageSrc)}
+      alt={alt || "아바타 이미지"}
       className="rounded-circle"
       style={{ objectFit: "cover", border: "2px solid #f000f0", width: `${diameter}px`, height: `${diameter}px` }}
       onError={handleImageLoadFailed}
