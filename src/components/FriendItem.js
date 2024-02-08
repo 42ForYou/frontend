54a42268f364f77eda48abc5c1d3c1a443cbfe68
yonaMac <yonaimyo@gmail.com>
2024-02-08 20:40:47 +0900

    FY-120/feat: 친구 아이템 작성

--- src/components/FriendItem.js
+++ src/components/FriendItem.js
@@ -1,10 +1,27 @@
 import React from "react";
-import StyledButton from "./styledButton";
+import Avatar from "./Avatar";
+import StyledButton from "./StyledButton";
 
-const FriendItem = () => {
+// todo: 친구 수락 버튼 구현
+const FriendItem = ({ status, friend }) => {
+  const { intra_id, nickname, avatar, is_online } = friend;
+
   return (
     <div className="FriendItem border border-primary w-100 p-3">
-      <div className="row ps-3 pe-3">친구 정보</div>
+      <div className="row ps-3 pe-3">
+        <div className="col">
+          <Avatar
+            src={avatar}
+            alt={`친구 ${nickname}의 아바타`}
+            to={`/profile/users/${intra_id}`}
+            isOnline={is_online}
+          />
+        </div>
+        <div className="col">
+          {nickname}
+          {status === "pending" && <StyledButton styleType={"btn btn-primary"} name={"친구 수락"} />}
+        </div>
+      </div>
     </div>
   );
 };
