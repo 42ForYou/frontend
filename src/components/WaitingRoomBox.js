9df0a925e1d845c13d621d2c36782a1fb2bc4ab7
yonaMac <yonaimyo@gmail.com>
2024-02-08 14:46:18 +0900

    FY-126/fix: exitRoomRequest 고침

--- src/components/WaitingRoomBox.js
+++ src/components/WaitingRoomBox.js
@@ -146,7 +146,8 @@ const WaitingRoomBox = ({ gameData, roomData, playersData }) => {
 
   const exitRoomRequest = async () => {
     try {
-      const resData = await del(API_ENDPOINTS.ROOM(game_id));
+      const player_id = 1; // todo: 본인의 플레이어 아이디를 넘겨야 함
+      const resData = await del(API_ENDPOINTS.PLAYERS(player_id));
       console.log("방 나가기 성공: ", resData);
     } catch (error) {
       console.log("방 나가기 요청 에러: ", error);
