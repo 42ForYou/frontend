41358f7bb0166e29983b58b6735b915f67e7af0f
yonaMac <yonaimyo@gmail.com>
2024-02-09 17:46:05 +0900

    FY-131/fix: 유저 리스트 페이지 필터 타입 all만 둠

--- src/components/UserListBox.js
+++ src/components/UserListBox.js
@@ -4,12 +4,14 @@ import { API_ENDPOINTS } from "../common/apiEndpoints";
 import UserItem from "./UserItem";
 
 const UserListBox = () => {
+  const filterTypes = [{ value: "all", label: "All" }];
+
   return (
     <>
       <ListBox
         apiEndpoint={(_, __, ___, searchKeyword) => API_ENDPOINTS.USER_SEARCH(searchKeyword)}
         ItemComponent={UserItem}
-        filterTypes={[]}
+        filterTypes={filterTypes}
         additionalButton={null}
         emptyMsg="검색 결과가 없습니다."
         searchable={true}
