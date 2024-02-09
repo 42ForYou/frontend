68a52e0c54faac114842533c938faf3543d74fad
yonaMac <yonaimyo@gmail.com>
2024-02-10 03:33:57 +0900

    FY-131/perf: 커스텀 훅 용도에 따라 로직 변경

--- src/hooks/useFetchItems.js
+++ src/hooks/useFetchItems.js
@@ -1,31 +1,36 @@
 import { useState, useEffect } from "react";
 import { get } from "../common/apiBase";
 
-const useFetchItems = (apiEndpoint, currentFilter, currentPage, searchKeyword, fetchLimit) => {
+// 필터, 페이지, 키워드를 넘겨받아 API를 호출하고 결과를 반환하는 커스텀 훅
+const useFetchListItems = (apiEndpoint, currentFilter, currentPage, searchKeyword, fetchLimit) => {
   const [itemsData, setItemsData] = useState(null);
   const [totalPage, setTotalPage] = useState(1);
   const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
+  const [error, setError] = useState({ code: null, msg: null });
 
   useEffect(() => {
     const fetchItems = async () => {
       setIsLoading(true);
+      setError({ code: null, msg: null });
       try {
         const resData = await get(apiEndpoint(currentFilter.value, currentPage, fetchLimit, searchKeyword));
         setItemsData(resData.data);
         setTotalPage(resData.pages.total_pages);
+        setIsLoading(false);
       } catch (error) {
         setItemsData(null);
-        setError(error);
-      } finally {
+        setError({
+          code: error.response?.status,
+          msg: error.response?.data.message || error.message,
+        });
         setIsLoading(false);
       }
     };
 
     fetchItems();
-  }, [apiEndpoint, currentFilter.value, currentPage, searchKeyword]);
+  }, [apiEndpoint, currentFilter.value, currentPage, searchKeyword, fetchLimit]);
 
   return { itemsData, totalPage, isLoading, error };
 };
 
-export default useFetchItems;
+export default useFetchListItems;
--- src/hooks/useFetchProfileData.js
+++ src/hooks/useFetchProfileData.js
@@ -1,21 +1,27 @@
-import { useState, useEffect, useContext } from "react";
+import { useState, useEffect } from "react";
 import { get } from "../common/apiBase";
 import { API_ENDPOINTS } from "../common/apiEndpoints";
 
+// 특정 사용자의 프로필 데이터를 비동기적으로 가져오는 커스텀 훅
+// 이 훅을 사용하는 컴포넌트가 마운트될 때 자동으로 데이터를 가져와야 하므로 useEffect를 사용
 const useFetchProfileData = (intraId) => {
   const [profileData, setProfileData] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
-  const [error, setError] = useState(null);
+  const [error, setError] = useState({ errcode: null, errmsg: null });
 
   useEffect(() => {
     const fetchProfileData = async () => {
       setIsLoading(true);
+      setError({ code: null, msg: null });
       try {
         const resData = await get(API_ENDPOINTS.USER_PROFILE(intraId));
         setProfileData(resData.data.user);
         setIsLoading(false);
       } catch (error) {
-        setError(error);
+        setError({
+          code: error.response?.status,
+          msg: error.response?.data.message || error.message,
+        });
         setIsLoading(false);
       }
     };
