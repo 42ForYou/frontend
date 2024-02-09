e486228a401cb1fbe8d3e7ef3147fe634d86b630
yonaMac <yonaimyo@gmail.com>
2024-02-09 14:36:30 +0900

    FY-134/feat: 유저 페이지 구현

--- src/pages/UsersPage.js
+++ src/pages/UsersPage.js
@@ -1,11 +1,14 @@
 import React from "react";
 import PageContainer from "../components/PageContainer";
 import withAuthProtection from "../common/withAuthProtection";
+import UserListBox from "../components/UserListBox";
 
 const UsersPage = () => {
   return (
     <div className="UsersPage">
-      <PageContainer hasNavigationBar={true}>This is UsersPage</PageContainer>
+      <PageContainer hasNavigationBar={true}>
+        <UserListBox />
+      </PageContainer>
     </div>
   );
 };
