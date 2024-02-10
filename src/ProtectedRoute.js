cf761bab8b84fcf73ce6e7cd7ec5ca2a9f324750
yonaMac <yonaimyo@gmail.com>
2024-02-11 00:51:05 +0900

    FY-146/fix: ProtectedRoute 오류 수정

--- src/ProtectedRoute.js
+++ src/ProtectedRoute.js
@@ -3,13 +3,12 @@ import { Navigate } from "react-router-dom";
 import { useAuth } from "./context/AuthContext";
 
 const ProtectedRoute = ({ children }) => {
-  const { loggedInUser } = useAuth();
+  const { loggedIn } = useAuth();
 
-  if (!loggedInUser) {
+  if (!loggedIn) {
     alert("로그인이 필요합니다.");
     return <Navigate to="/login" />;
   }
-
   return children;
 };
 
