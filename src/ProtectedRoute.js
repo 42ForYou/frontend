40a8f9b1855d7cd2143222c4e5742452b23cf4eb
yonaMac <yonaimyo@gmail.com>
2024-02-11 01:10:00 +0900

    FY-146/feat: 보호된 라우트 컴포넌트 로딩 상태 추가
    - 유효한 토큰을 갖고있는 상태에서 새로고침 한 경우에도 정상 접근 가능하게 됨

--- src/ProtectedRoute.js
+++ src/ProtectedRoute.js
@@ -3,12 +3,18 @@ import { Navigate } from "react-router-dom";
 import { useAuth } from "./context/AuthContext";
 
 const ProtectedRoute = ({ children }) => {
-  const { loggedIn } = useAuth();
+  const { loggedIn, isLoading } = useAuth();
+
+  // todo: 로딩 인디케이터 컴포넌트 추가
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
 
   if (!loggedIn) {
     alert("로그인이 필요합니다.");
     return <Navigate to="/login" />;
   }
+
   return children;
 };
 
