ff79e75a91ffd18722400628078d77100c981751
yonaMac <yonaimyo@gmail.com>
2024-02-11 01:11:45 +0900

    FY-146/chore: ProtectedRoute 컴포넌트 파일 옮김

--- src/components/auth/ProtectedRoute.js
+++ src/components/auth/ProtectedRoute.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Navigate } from "react-router-dom";
-import { useAuth } from "./context/AuthContext";
+import { useAuth } from "../../context/AuthContext";
 
 const ProtectedRoute = ({ children }) => {
   const { loggedIn, isLoading } = useAuth();
