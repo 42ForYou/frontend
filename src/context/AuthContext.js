dbd9206d4989fc6eb8c0f6a7b612b31d398e62ae
yonaMac <yonaimyo@gmail.com>
2024-02-11 00:32:43 +0900

    FY-146/feat: 로그인 시 쿠키 안 토큰 검사 추가

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,36 +1,13 @@
 import React, { createContext, useState, useContext, useEffect } from "react";
 import { get } from "../common/apiBase";
 import { API_ENDPOINTS } from "../common/apiEndpoints";
+
 const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
-  const [isLoading, setIsLoading] = useState(true); // 로딩 상태
+  const [isLoading, setIsLoading] = useState(true);
   const [loggedIn, setLoggedIn] = useState(null);
 
-  const login = async (code) => {
-    try {
-      const resData = await get(API_ENDPOINTS.OAUTH_REDIRECT(code));
-      if (resData) {
-        console.log("로그인 성공");
-        const { user } = resData.data;
-        setLoggedIn(user);
-        setIsLoading(false);
-        return true;
-      } else {
-        console.log("로그인 실패");
-      }
-    } catch (error) {
-      console.error("로그인 중 에러 발생: ", error);
-    }
-    return false;
-  };
-
-  const logout = async () => {
-    // todo: 쿠키 삭제 API 구현 후 변경
-    setLoggedIn(null);
-  };
-
-  // 현재 쿠키에 저장되어 있는 토큰의 유효성을 검사
   const validateTokenInCookies = async () => {
     try {
       const resData = await get(API_ENDPOINTS.VALID);
@@ -51,6 +28,26 @@ export const AuthProvider = ({ children }) => {
     validateTokenInCookies();
   }, []);
 
+  const login = async (code) => {
+    if (await validateTokenInCookies()) return true;
+    try {
+      const resData = await get(API_ENDPOINTS.OAUTH_REDIRECT(code));
+      const { user } = resData.data;
+      setLoggedIn(user);
+      setIsLoading(false);
+      console.log("로그인 성공");
+    } catch (error) {
+      console.error("로그인 중 에러 발생: ", error);
+      return false;
+    }
+    return true;
+  };
+
+  // todo: 쿠키 삭제 API 구현 후 변경
+  const logout = async () => {
+    setLoggedIn(null);
+  };
+
   return (
     <AuthContext.Provider
       value={{
@@ -66,7 +63,6 @@ export const AuthProvider = ({ children }) => {
   );
 };
 
-// AuthContext를 사용하기 위한 커스텀 훅
 export const useAuth = () => useContext(AuthContext);
 
 export default AuthContext;
