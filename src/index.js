0b1ec862144f5e7d9da4226ec4fda14f82d274ca
yonaMac <yonaimyo@gmail.com>
2024-02-11 18:26:46 +0900

    FY-139/fix: main.css 싸스 파일로 변경하여 번들링 문제 해결

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "@fortawesome/fontawesome-free/js/all.js";
-import "./style/main.css";
+import "./style/main.scss";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
