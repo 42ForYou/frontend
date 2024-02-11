9e10b0f52f04db638dccfb1097641b608eb63798
JinMu Go <jingmuio@gmail.com>
2024-02-11 21:04:30 +0900

    FY-147/feature project docker env refactor (#32)
    * chore(webpack): split .env into prod, dev
    
    * rename(dockerfile): dockerfile.prod
    
    * chore(.ignore): add ignore to .env files

--- config/webpack/webpack.prod.js
+++ config/webpack/webpack.prod.js
@@ -1,6 +1,12 @@
 const { merge } = require("webpack-merge");
 const common = require("./webpack.common.js");
+const DotenvWebpackPlugin = require("dotenv-webpack");
 
 module.exports = merge(common, {
   mode: "production",
+  plugins: [
+    new DotenvWebpackPlugin({
+      path: "./.env.prod",
+    }),
+  ],
 });
