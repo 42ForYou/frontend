4d89ebd4cea17434f692f41fb99d44b7ae87004b
yonaMac <yonaimyo@gmail.com>
2024-02-11 16:08:22 +0900

    FY-139/refactor: ListItems 컴포넌트 리팩토링 및 스타일링 수정

--- src/components/list/ListItems.js
+++ src/components/list/ListItems.js
@@ -1,36 +1,39 @@
 import React from "react";
 
+const ListItemRow = ({ itemRow, ItemComponent, onOccurChange }) => (
+  <div className="flex-grow-1 d-flex">
+    {itemRow.map((item, itemIndex) => (
+      <div className="flex-grow-1" key={itemIndex}>
+        <ItemComponent {...item} onOccurChange={onOccurChange} />
+      </div>
+    ))}
+  </div>
+);
+
 const ListItems = ({ itemsData, ItemComponent, itemsPerRow, emptyMsg, onOccurChange }) => {
-  // 1차 배열을 행 단위로 나눠 2차 배열로 반환
-  const divideArrayInto2DRows = (data, itemsPerRow) => {
-    const rows = [];
+  // 데이터를 행 단위로 나누어 2차원 배열로 반환하는 함수
+  const createItemRows = (data, itemsPerRow) => {
+    const itemRows = [];
     if (data) {
       for (let i = 0; i < data.length; i += itemsPerRow) {
-        rows.push(data.slice(i, i + itemsPerRow));
+        itemRows.push(data.slice(i, i + itemsPerRow));
       }
     }
-    return rows;
+    return itemRows;
   };
 
-  const rows = divideArrayInto2DRows(itemsData, itemsPerRow);
+  const itemRows = createItemRows(itemsData, itemsPerRow);
+  const loadFailMsg = "데이터 로드에 실패했습니다.";
+  const displayMessage = !itemsData ? loadFailMsg : itemsData.length === 0 ? emptyMsg : null;
 
   return (
-    <div className="ListItems flex-grow-1">
-      {rows.map((row, rowIndex) => (
-        <div className="row mb-3" key={rowIndex}>
-          {row.map((item, itemIndex) => (
-            <div className={`col-${12 / itemsPerRow}`} key={itemIndex}>
-              <ItemComponent {...item} onOccurChange={onOccurChange} />
-            </div>
-          ))}
-        </div>
+    <div className="ListItems flex-grow-1 d-flex flex-column">
+      {itemRows.map((itemRow, rowIndex) => (
+        <ListItemRow itemRow={itemRow} ItemComponent={ItemComponent} onOccurChange={onOccurChange} key={rowIndex} />
       ))}
-      <div className="row mb-3">
-        <div className="d-flex justify-content-center align-items-center text-center">
-          {!itemsData && "데이터 로드에 실패했습니다."}
-          {itemsData && itemsData.length === 0 && emptyMsg}
-        </div>
-      </div>
+      {displayMessage && (
+        <div className="flex-grow-1 d-flex justify-content-center align-items-center text-center">{displayMessage}</div>
+      )}
     </div>
   );
 };
