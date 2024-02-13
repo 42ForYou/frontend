0e37b1491dcebce9836ee25f00feb3336f9cec45
yonaMac <yonaimyo@gmail.com>
2024-02-13 21:39:10 +0900

    FY-155/feat: SocketContext 추가
    - SOCKET_URL 사용
    - 클라이언트당 하나의 소켓만 생성

---
+++ src/context/SocketContext.js
@@ -0,0 +1,21 @@
+import React, { createContext, useContext, useState, useEffect } from "react";
+import io from "socket.io-client";
+
+const SocketContext = createContext();
+
+export const SocketProvider = ({ children }) => {
+  const [socket, setSocket] = useState(null);
+
+  useEffect(() => {
+    const socketInstance = io(process.env.SOCKET_URL);
+    setSocket(socketInstance);
+
+    return () => {
+      socketInstance.disconnect();
+    };
+  }, []);
+
+  return <SocketContext.Provider value={socket}>{children}</SocketContext.Provider>;
+};
+
+export const useSocket = () => useContext(SocketContext);
