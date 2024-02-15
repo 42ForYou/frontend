5864287254ad2645f540c1c44f368a367a947d3f
yonaMac <yonaimyo@gmail.com>
2024-02-15 15:51:24 +0900

    FY-165/feat: useWaitingRoomDataSync 커스텀 훅에 소켓 네임스페이스 적용

--- src/hooks/useWaitingRoomDataSync.js
+++ src/hooks/useWaitingRoomDataSync.js
@@ -1,50 +1,59 @@
 import { useEffect, useState } from "react";
 import { useSocket } from "../context/SocketContext";
+import { get } from "../utils/apiBase";
+import { API_ENDPOINTS } from "../utils/apiEndpoints";
 
 const useWaitingRoomDataSync = (roomId) => {
-  const socket = useSocket();
+  const { sockets, connectNamespace, disconnectNamespace, setupEventListeners, removeEventListeners } = useSocket();
   const [gameData, setGameData] = useState(null);
   const [roomData, setRoomData] = useState(null);
   const [playersData, setPlayersData] = useState(null);
   const [myPlayerId, setMyPlayerId] = useState(null);
+  const namespace = `/game/room/${roomId}`;
+
+  const setNewRoomData = (newRoomData) => {
+    const { game, room, players, my_player_id } = newRoomData;
+    setGameData(game);
+    setRoomData(room);
+    setPlayersData(players);
+    setMyPlayerId(my_player_id);
+  };
 
   useEffect(() => {
+    connectNamespace(namespace);
+
     const fetchWaitingRoomData = async (roomId) => {
       try {
         const resData = await get(API_ENDPOINTS.ROOM(roomId));
-        const { game, room, players, my_player_id } = resData.data;
-        setGameData(game);
-        setRoomData(room);
-        setPlayersData(players);
-        setMyPlayerId(my_player_id);
-        console.log(resData);
+        setNewRoomData(resData.data);
       } catch (error) {
         console.log("방 데이터 로드 실패: ", error);
       }
     };
+
     fetchWaitingRoomData(roomId);
-  }, []);
+  }, [namespace]);
 
   useEffect(() => {
-    if (!socket) return;
-
-    // 방 입장 소켓 이벤트 있을시 여기서 emit
-
-    const handleRoomUpdated = (data) => {
-      const { game, room, players, my_player_id } = data;
-      setGameData(game);
-      setRoomData(room);
-      setPlayersData(players);
-      setMyPlayerId(my_player_id);
-    };
-
-    socket.on("info_room", handleRoomUpdated);
+    if (!sockets[namespace]) return;
+    const eventListeners = [
+      {
+        event: "update_room",
+        handler: (data) => {
+          setNewRoomData(data);
+        },
+      },
+    ];
+    setupEventListeners(namespace, eventListeners);
 
     return () => {
-      // 방 퇴장 소켓 이벤트 있을시 여기서 emit
-      socket.off("info_room", handleRoomUpdated);
+      removeEventListeners(
+        namespace,
+        eventListeners.map((listener) => listener.event)
+      );
+      disconnectNamespace(namespace);
     };
-  }, [socket, roomId]);
+  }, [sockets, namespace]);
 
   return { gameData, roomData, playersData, myPlayerId };
 };
