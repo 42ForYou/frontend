6cd910fa6225c8c1b7e73a1b912b22103d10a4af
yonaMac <yonaimyo@gmail.com>
2024-02-16 03:34:34 +0900

    FY-167/feat: 게임 시작 버튼 클릭시 게임 플레이 페이지로 리다이렉트

--- src/components/waiting_room/StartGameButton.js
+++ src/components/waiting_room/StartGameButton.js
@@ -2,15 +2,18 @@ import React from "react";
 import StyledButton from "../common/StyledButton";
 import { useTournament } from "../../context/TournamentContext";
 import { useSocket } from "../../context/SocketContext";
+import { useNavigate } from "react-router-dom";
 
 const StartGameButton = ({ isActive }) => {
-  const { roomData } = useTournament();
+  const navigate = useNavigate();
+  const { roomData, gameData } = useTournament();
   const namespace = `/game/room/${roomData.id}`;
   const socket = useSocket().sockets[namespace];
 
   const handleStartGame = () => {
     if (!window.confirm("게임을 시작하시겠습니까?")) return;
     socket.emitWithTime("start");
+    navigate(`/game/play/${gameData.game_id}`);
   };
 
   return (
