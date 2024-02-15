2a88d91db87757c859c3eb326a64f08a3b8a3f0e
yonaMac <yonaimyo@gmail.com>
2024-02-15 21:01:07 +0900

    FY-167/feat: 대기방 입장 후 소켓 연결 실패시 플레이어 삭제 요청 날림

--- src/pages/GameWaitingRoomPage.js
+++ src/pages/GameWaitingRoomPage.js
@@ -1,15 +1,19 @@
 import React, { useEffect } from "react";
 import LoadingPage from "./LoadingPage";
 import WaitingRoomBox from "../components/waiting_room/WaitingRoomBox";
-import { useNavigate, useParams } from "react-router-dom";
+import { useNavigate, useParams, useLocation } from "react-router-dom";
 import useWaitingRoomDataSync from "../hooks/useWaitingRoomDataSync";
 import { useSocket } from "../context/SocketContext";
+import { del } from "../utils/apiBase";
+import { API_ENDPOINTS } from "../utils/apiEndpoints";
 
 // 차후 필요시 1대1, 토너먼트 방 분리
 const GameWaitingRoomPage = () => {
+  const location = useLocation();
+  const myPlayerId = location.state.myPlayerId;
   const navigate = useNavigate();
   const { room_id: roomId } = useParams();
-  const { gameData, roomData, playersData, myPlayerId, isLoading, loadError } = useWaitingRoomDataSync(roomId);
+  const { gameData, roomData, playersData, isLoading, loadError } = useWaitingRoomDataSync(roomId);
   const { connectNamespace, disconnectNamespace, sockets, setupEventListeners, removeEventListeners } = useSocket();
   const namespace = `/game/room/${roomId}`;
 
@@ -18,13 +22,24 @@ const GameWaitingRoomPage = () => {
     navigate(-1);
   };
 
+  // todo: 소켓 통신 연결 실패하고 방 나가기 요청도 실패할시 처리가 안됨
+  const handleExitWaitingRoom = async () => {
+    try {
+      const resData = await del(API_ENDPOINTS.PLAYERS(myPlayerId));
+      navigate(-1);
+      console.log("방 나가기 성공", resData);
+    } catch (error) {
+      console.log("방 나가기 요청 실패: ", error);
+    }
+  };
+
   useEffect(() => {
     connectNamespace(namespace, {
       onConnect: () => console.log(`${namespace} connected`),
       onConnectError: (err) => {
         console.error("소켓 연결 에러", err);
         alert("실시간 통신 연결에 실패하였습니다.");
-        navigate(-1);
+        handleExitWaitingRoom();
       },
       onDisconnect: (reason) => console.log(`${namespace} disconnected`, reason),
     });
@@ -40,7 +55,7 @@ const GameWaitingRoomPage = () => {
   useEffect(() => {
     if (!isLoading && loadError) {
       alert("방 정보를 불러오는데 실패했습니다.");
-      navigate(-1);
+      handleExitWaitingRoom();
     }
   }, [isLoading, loadError, navigate]);
 
