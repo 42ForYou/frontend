1747159304667ced20dbfcb03c7b946ab77b987c
yonaMac <yonaimyo@gmail.com>
2024-02-16 12:25:13 +0900

    FY-156/refactor: 대진표 컴포넌트 소켓 처리 삭제

--- src/components/game/GameOngoing.js
+++ src/components/game/GameOngoing.js
@@ -18,20 +18,9 @@ const roundsData = [
 
 const GameOngoing = () => {
   const [isPlaying, setIsPlaying] = useState(false);
-  const socket = useSocket();
 
-  // 컴포넌트 마운트 후 수신할 이벤트를 등록
-  useEffect(() => {
-    if (socket) {
-      socket.on("info_tournament", (data) => {
-        // todo: 대진표 정보를 받아와 렌더링
-      });
-      // 컴포넌트가 언마운트 되기 전에 이벤트 리스너를 제거
-      return () => {
-        socket.off("info_tournament");
-      };
-    }
-  }, []);
+  // 컴포넌트 마운트 후 소켓 수신할 이벤트를 등록
+  useEffect(() => {}, []);
 
   return <div>{isPlaying ? <PongScene /> : <TournamentBracket rounds={roundsData} />}</div>;
 };
