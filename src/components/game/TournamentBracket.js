3b3d92a2cea351026aa0f4e8c8202f5f63b326c2
yonaMac <yonaimyo@gmail.com>
2024-02-16 12:25:38 +0900

    FY-156/feat: Bracket 컴포넌트 초안 작성

--- src/components/game/TournamentBracket.js
+++ src/components/game/TournamentBracket.js
@@ -1,18 +1,23 @@
 import React from "react";
 import Avatar from "../common/Avatar";
 
-const BracketPlayer = ({ name, avatar, transform }) => {
+const BracketPlayer = ({ name, avatar, columnCnt, nthColumn, topPos }) => {
+  const diameter = 130;
+  const leftPos = (nthColumn / columnCnt) * 100;
+
   return (
-    <div
-      className="bracket-player"
-      style={{ position: "absolute", top: "50%", left: "50%", transform: "translate(-50%, -50%)" }}>
-      <Avatar diameter={130} />
-      <p>{name}</p>
+    <div className="bracket-player p-0" style={{ position: "absolute", top: `${topPos}%`, left: `${leftPos}%` }}>
+      <div
+        className={"text-center"}
+        style={{ width: `${diameter}px`, height: `${diameter}px`, transform: "translate(-50%, -50%)" }}>
+        <Avatar src={avatar} diameter={130} />
+        <p>{name}</p>
+      </div>
     </div>
   );
 };
 
-const Bracket = () => (
+const Bracket = ({ players }) => (
   <div className="bracket container-fluid p-0">
     <div className="row">
       <div className="col bracket-box" style={{ borderRight: "1px solid red" }}></div>
@@ -23,17 +28,8 @@ const Bracket = () => (
       <div className="col bracket-box" style={{ borderLeft: "1px solid red", borderTop: "1px solid red" }}></div>
       <div className="col bracket-box" style={{ borderRight: "1px solid red", borderTop: "1px solid red" }}></div>
       <div className="col bracket-box"></div>
-      <BracketPlayer name="Player 1" transform="translate(-50%, -50%)" />
-      {/* <img
-        src="https://png.pngtree.com/png-clipart/20201031/ourmid/pngtree-circle-clipart-lake-blue-png-image_2382137.jpg"
-        alt="Floating Image"
-        style={{ position: "absolute", top: "50%", left: "50%", transform: "translate(-75%, -50%)" }}
-      />
-      <img
-        src="https://png.pngtree.com/png-clipart/20201031/ourmid/pngtree-circle-clipart-lake-blue-png-image_2382137.jpg"
-        alt="Floating Image"
-        style={{ position: "absolute", top: "50%", left: "50%", transform: "translate(-25%, -50%)" }}
-      /> */}
+      <BracketPlayer name="Player 1" columnCnt={4} nthColumn={1} topPos={75} />
+      <BracketPlayer name="Player 2" columnCnt={4} nthColumn={3} topPos={75} />
     </div>
     <div className="row">
       <div className="col bracket-box"></div>
@@ -45,11 +41,15 @@ const Bracket = () => (
       <div className="col bracket-box" style={{ borderRight: "1px solid red", borderTop: "1px solid red" }}></div>
       <div className="col bracket-box"></div>
     </div>
-    <div className="row">
+    <div className="row" style={{ position: "relative" }}>
       <div className="col bracket-box"></div>
       <div className="col bracket-box"></div>
       <div className="col bracket-box"></div>
       <div className="col bracket-box"></div>
+      <BracketPlayer name="Player 1" columnCnt={8} nthColumn={1} topPos={0} />
+      <BracketPlayer name="Player 1" columnCnt={8} nthColumn={3} topPos={0} />
+      <BracketPlayer name="Player 1" columnCnt={8} nthColumn={5} topPos={0} />
+      <BracketPlayer name="Player 1" columnCnt={8} nthColumn={7} topPos={0} />
     </div>
   </div>
 );
