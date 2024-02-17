73997e4da637fd64add3333e4b902dba167168fc
yonaMac <yonaimyo@gmail.com>
2024-02-18 02:24:19 +0900

    FY-156/refactor: Bracket4Players 컴포넌트 분리

--- src/components/game/Bracket.js
+++ src/components/game/Bracket.js
@@ -18,42 +18,65 @@ const BracketPlayer = ({ playerData, columnCnt, nthColumn, topPos }) => {
   );
 };
 
+const BracketBoxNonBorder = () => {
+  return <div className="col bracket-box"></div>;
+};
+
+const BracketBoxLeftTopBorder = () => {
+  return <div className="col bracket-box" style={{ borderLeft: "3px solid red", borderTop: "3px solid red" }}></div>;
+};
+
+const BracketBoxRightTopBorder = () => {
+  return <div className="col bracket-box" style={{ borderRight: "3px solid red", borderTop: "3px solid red" }}></div>;
+};
+
+const BracketRow = ({ children }) => {
+  return (
+    <div className="row" style={{ position: "relative" }}>
+      {children}
+    </div>
+  );
+};
+
 const Bracket4Players = ({ players }) => (
   <>
     <div className="bracket container-fluid p-0">
-      <div className="row" style={{ position: "relative" }}>
+      <BracketRow>
         <BracketPlayer playerData={{ nickname: "winner" }} columnCnt={2} nthColumn={1} topPos={75} />
-        <div className="col bracket-box"></div>
-        <div className="col bracket-box"></div>
-      </div>
-      <div className="row" style={{ position: "relative" }}>
-        <div className="col bracket-box"></div>
-        <div className="col bracket-box" style={{ borderLeft: "3px solid red", borderTop: "3px solid red" }}></div>
-        <div className="col bracket-box" style={{ borderRight: "3px solid red", borderTop: "3px solid red" }}></div>
-        <div className="col bracket-box"></div>
+        <BracketBoxNonBorder />
+        <BracketBoxNonBorder />
+      </BracketRow>
+
+      <BracketRow>
         <BracketPlayer playerData={players[0][0][0]} columnCnt={4} nthColumn={1} topPos={75} />
         <BracketPlayer playerData={players[0][0][1]} columnCnt={4} nthColumn={3} topPos={75} />
-      </div>
-      <div className="row">
-        <div className="col bracket-box"></div>
-        <div className="col bracket-box" style={{ borderLeft: "3px solid red", borderTop: "3px solid red" }}></div>
-        <div className="col bracket-box" style={{ borderRight: "3px solid red", borderTop: "3px solid red" }}></div>
-        <div className="col bracket-box"></div>
-        <div className="col bracket-box"></div>
-        <div className="col bracket-box" style={{ borderLeft: "3px solid red", borderTop: "3px solid red" }}></div>
-        <div className="col bracket-box" style={{ borderRight: "3px solid red", borderTop: "3px solid red" }}></div>
-        <div className="col bracket-box"></div>
-      </div>
-      <div className="row" style={{ position: "relative" }}>
-        <div className="col bracket-box"></div>
-        <div className="col bracket-box"></div>
-        <div className="col bracket-box"></div>
-        <div className="col bracket-box"></div>
+        <BracketBoxNonBorder />
+        <BracketBoxLeftTopBorder />
+        <BracketBoxRightTopBorder />
+        <BracketBoxNonBorder />
+      </BracketRow>
+
+      <BracketRow>
+        <BracketBoxNonBorder />
+        <BracketBoxLeftTopBorder />
+        <BracketBoxRightTopBorder />
+        <BracketBoxNonBorder />
+        <BracketBoxNonBorder />
+        <BracketBoxLeftTopBorder />
+        <BracketBoxRightTopBorder />
+        <BracketBoxNonBorder />
+      </BracketRow>
+
+      <BracketRow>
         <BracketPlayer playerData={players[1][0][0]} columnCnt={8} nthColumn={1} topPos={0} />
         <BracketPlayer playerData={players[1][0][1]} columnCnt={8} nthColumn={3} topPos={0} />
         <BracketPlayer playerData={players[1][1][0]} columnCnt={8} nthColumn={5} topPos={0} />
         <BracketPlayer playerData={players[1][1][1]} columnCnt={8} nthColumn={7} topPos={0} />
-      </div>
+        <BracketBoxNonBorder />
+        <BracketBoxNonBorder />
+        <BracketBoxNonBorder />
+        <BracketBoxNonBorder />
+      </BracketRow>
     </div>
   </>
 );
