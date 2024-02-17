cb9c050badd2f5ec3a80d8bfe84474e5b18f98e4
yonaMac <yonaimyo@gmail.com>
2024-02-18 02:16:00 +0900

    FY-156/refactor: 컴포넌트 명 변경 GamePlaying

--- src/pages/GamePlayPage.js
+++ src/pages/GamePlayPage.js
@@ -1,13 +1,13 @@
 import React from "react";
 import ContentContainer from "../components/layout/ContentContainer";
-import GameOngoing from "../components/game/GameOngoing";
+import GamePlaying from "../components/game/GamePlaying";
 
 // todo: 해당 게임에 참가하지 않는 유저가 접속 시도시 리다이렉팅
 // todo: 현 상태에 따라 대진표 혹은 게임 플레이 화면을 보여줌
 const GamePlayPage = () => {
   return (
     <div className="GamePlayPage">
-      <GameOngoing />
+      <GamePlaying />
     </div>
   );
 };
