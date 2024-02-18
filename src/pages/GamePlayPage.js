3a1734c99b85127481c941c0a433307a725a2d1c
yonaMac <yonaimyo@gmail.com>
2024-02-18 16:15:50 +0900

    FY-171/feat: 게임 플레이 페이지 start, config 이벤트 리슨 추가

--- src/pages/GamePlayPage.js
+++ src/pages/GamePlayPage.js
@@ -67,29 +67,51 @@ const BracketPage = () => {
 
 const GamePlayPage = () => {
   const navigate = useNavigate();
-  const { roomData, myPlayerId, resetTournamentData, setBracketData } = useTournament();
-  const { connectNamespace, disconnectNamespace, setupEventListeners, removeEventListeners } = useSockek();
-  const { sockets } = useSocket();
+  const { roomData, myPlayerId, resetTournamentData, setBracketData, setMatchData, matchData } = useTournament();
+  const { connectNamespace, disconnectNamespace, setupEventListeners, removeEventListeners, sockets } = useSocket();
   const roomNamespace = `/game/room/${roomData?.id}`;
+  const [matchNamespace, setMatchNamespace] = useState("");
   const [showBracket, setShowBracket] = useState(true);
 
   const connectMatchSocket = (matchNamespace) => {
     connectNamespace(matchNamespace, {
-      onConnectError: (err) => {
+      onConnectError: () => {
         alert("실시간 통신 연결에 실패하였습니다.");
         handleAbortExit();
       },
-      onDisconnect: (reason) => {
+      onDisconnect: () => {
         resetTournamentData();
       },
     });
   };
 
+  const setupMatchEventListeners = (matchNamespace) => {
+    setupEventListeners(matchNamespace, [
+      {
+        event: "start",
+        handler: () => {
+          setShowBracket(false);
+        },
+      },
+      {
+        event: "config",
+        handler: (data) => {
+          setMatchConfig({ ...matchData, config: data });
+        },
+      },
+    ]);
+  };
+
   const updateBracketHandler = (data) => {
+    const rank = data.rank_ongoing;
+    const idx_in_rank = data.players.findIndex((rank) =>
+      rank.some((pair) => pair.some((player) => player.intra_id === myPlayerId))
+    );
+    const matchNamespace = `${roomNamespace}/${rank}/${idx_in_rank}`;
+    setMatchNamespace(matchNamespace);
+    connectMatchSocket(matchNamespace);
     setBracketData(data);
     setShowBracket(true);
-    const matchNamespace = `${roomNamespace}/${data.n_ranks}/${data.rank_ongoing}`;
-    connectMatchSocket(matchNamespace);
   };
 
   useEffect(() => {
@@ -118,13 +140,20 @@ const GamePlayPage = () => {
       // todo: 게임 플레이 중 나가는 경우 백측과 협의
       // sockets[].emitWithTime("leave", { my_player_id: myPlayerId });
     };
-
     window.addEventListener("beforeunload", handleBeforeUnload);
-
     return () => {
       window.removeEventListener("beforeunload", handleBeforeUnload);
     };
   }, []);
+
+  useEffect(() => {
+    setupMatchEventListeners(matchNamespace);
+
+    return () => {
+      removeEventListeners(matchNamespace, ["start", "config"]);
+      disconnectNamespace(matchNamespace);
+    };
+  }, sockets[matchNamespace]);
 
   return (
     <div className="GamePlayPage">
