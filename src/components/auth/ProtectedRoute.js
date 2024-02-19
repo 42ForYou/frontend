463dd1c328656229f7f4cb46e87d4e1939c784dd
yonaMac <yonaimyo@gmail.com>
2024-02-19 20:11:36 +0900

    FY-177/fix: 보호된 페이지 고차 컴포넌트 수정

--- src/components/auth/ProtectedRoute.js
+++ src/components/auth/ProtectedRoute.js
@@ -1,21 +1,31 @@
-import React from "react";
-import { Navigate, Outlet } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { useNavigate, Outlet } from "react-router-dom";
 import { useAuth } from "../../context/AuthContext";
 import LoadingPage from "../../pages/LoadingPage";
 
+// todo: 인증 컨텍스트로부터 로딩 상태 갖고오기
 const ProtectedRoute = () => {
-  const { loggedIn, isLoading, is2FAVerified } = useAuth();
+  const { loggedIn, validateTokenInCookies } = useAuth();
+  const navigate = useNavigate();
+  const [isLoading, setIsLoading] = useState(true);
 
-  if (process.env.NO_AUTH_PROTECTION === "true") return <Outlet />;
+  useEffect(() => {
+    const validateToken = async () => {
+      await validateTokenInCookies();
+      setIsLoading(false);
+    };
+    validateToken();
+  }, []);
 
-  // todo: 로딩 인디케이터 컴포넌트 추가
+  useEffect(() => {
+    if (!isLoading && !loggedIn) {
+      alert("로그인이 필요합니다.");
+      navigate("/login");
+    }
+  }, [isLoading, loggedIn, navigate]);
+
   if (isLoading) {
     return <LoadingPage />;
-  }
-
-  if (!loggedIn || !is2FAVerified) {
-    alert("로그인이 필요합니다.");
-    return <Navigate to="/login" />;
   }
 
   return <Outlet />;
