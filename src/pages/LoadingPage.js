d896fdd0bbd2182fa527c10fe27afb1851f84ae3
yonaMac <yonaimyo@gmail.com>
2024-02-19 21:14:31 +0900

    FY-177/style: 로딩 페이지 스피너 추가

--- src/pages/LoadingPage.js
+++ src/pages/LoadingPage.js
@@ -1,10 +1,16 @@
 import React from "react";
-import ContentContainer from "../components/layout/ContentContainer";
 
 const LoadingPage = () => {
   return (
-    <div className="LoadingPage text-center">
-      <ContentContainer>Loading ...</ContentContainer>
+    <div
+      className="LoadingPage text-center d-flex justify-content-center align-items-center"
+      style={{ height: "100vh" }}>
+      <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
+        <div className="spinner-border" role="status" style={{ width: "3rem", height: "3rem", color: "white" }}>
+          <span className="sr-only">Loading...</span>
+        </div>
+        <p style={{ color: "white", marginTop: "20px" }}>Loading...</p>
+      </div>
     </div>
   );
 };
