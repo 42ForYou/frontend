93f259ebdbb90041ed6b8b8f399e995f634cfeb3
yonaMac <yonaimyo@gmail.com>
2024-02-20 13:23:44 +0900

    FY-177/feat: 인증 컨텍스트 2FA 데이터 저장 추가

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,5 +1,5 @@
 import React, { createContext, useState, useContext, useEffect } from "react";
-import { get } from "../utils/apiBase";
+import { get, patch } from "../utils/apiBase";
 import { API_ENDPOINTS } from "../utils/apiEndpoints";
 
 const AuthContext = createContext();
@@ -8,7 +8,7 @@ export const AuthProvider = ({ children }) => {
   const [isLoading, setIsLoading] = useState(true);
   const [loggedIn, setLoggedIn] = useState(null);
   const [is2FA, setIs2FA] = useState(false);
-  const [dataFor2FA, setDataFor2FA] = useState({}); // {email, intra_id}
+  const [twoFactorData, setTwoFactorData] = useState(null); // {email, intra_id, two_factor_auth: true}
 
   // todo: 쿠키 삭제 API 구현 후 변경
   const logout = async () => {
@@ -22,11 +22,14 @@ export const AuthProvider = ({ children }) => {
       const resData = await get(API_ENDPOINTS.OAUTH_TOKEN_EXCHANGE(code));
       const user = resData.data.profile;
       setLoggedIn(user);
-      setIs2FA(false);
     } catch (error) {
       if (error.response?.status === 428) {
-        setIs2FA(true);
-        setDataFor2FA(error.response.data);
+        console.log("2FA 인증이 필요한 사용자입니다.");
+        setTwoFactorData({
+          email: error.response.data.data.email,
+          intra_id: error.response.data.data.intra_id,
+          two_factor_auth: true,
+        });
       }
     } finally {
       setIsLoading(false);
@@ -41,7 +44,7 @@ export const AuthProvider = ({ children }) => {
       setLoggedIn(user);
       setIs2FA(user.two_factor_auth);
     } catch (error) {
-      console.log("쿠키에 저장된 토큰 유효성 검사 실패: ", error);
+      console.log("쿠키에 저장된 토큰이 유효하지 않습니다.");
       setLoggedIn(null);
     } finally {
       setIsLoading(false);
@@ -49,11 +52,13 @@ export const AuthProvider = ({ children }) => {
   };
 
   const resend2FACode = async () => {
-    await patch(API_ENDPOINTS.TWO_FA(), { email: dataFor2FA.email, intra_id: dataFor2FA.intra_id });
+    await patch(API_ENDPOINTS.VALIDATE_2FA(), { email: twoFactorData.email, intra_id: twoFactorData.intra_id });
   };
 
   const validate2FAcode = async (code2FA) => {
-    const resData = await get(API_ENDPOINTS.TWO_FA(dataFor2FA.intra_id, code2FA));
+    const resData = await get(API_ENDPOINTS.VALIDATE_2FA(twoFactorData.intra_id, code2FA));
+    console.log("2FA 인증 성공");
+    console.log(resData);
     setLoggedIn(resData.data.profile);
   };
 
@@ -68,6 +73,7 @@ export const AuthProvider = ({ children }) => {
         authenticateWithOAuth,
         is2FA,
         resend2FACode,
+        twoFactorData,
       }}>
       {children}
     </AuthContext.Provider>
