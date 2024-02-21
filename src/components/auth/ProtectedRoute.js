2964b09be9df8f2b39185fff4d595752b48aa35b
yonaMac <yonaimyo@gmail.com>
2024-02-22 05:06:07 +0900

    FY-158/feat: ProtectedRoute 컴포넌트에 NO_AUTH 옵션 추가

--- src/components/auth/ProtectedRoute.js
+++ src/components/auth/ProtectedRoute.js
@@ -4,6 +4,8 @@ import { useAuth } from "../../context/AuthContext";
 import LoadingPage from "../../pages/LoadingPage";
 
 const ProtectedRoute = () => {
+  if (process.env.NO_AUTH_PROTECTION === "true") return <Outlet />;
+
   const { loggedIn, isLoading, validateTokenInCookies } = useAuth();
   const navigate = useNavigate();
 
