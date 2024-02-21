095a1894fff7133bd8386da09586393eb5084e0a
yonaMac <yonaimyo@gmail.com>
2024-02-22 00:22:11 +0900

    FY-171/refactor: myPlayerId -> myPlayerData 사용 변

--- src/components/waiting_room/ExitRoomButton.js
+++ src/components/waiting_room/ExitRoomButton.js
@@ -6,20 +6,15 @@ import { useSocket } from "../../context/SocketContext";
 
 const ExitRoomButton = () => {
   const navigate = useNavigate();
-  const { roomData, myPlayerId } = useGame();
+  const { roomData, myPlayerData } = useGame();
   const namespace = `/game/room/${roomData?.id}`;
   const { sockets } = useSocket();
 
   const handleNormalExit = async () => {
     if (!window.confirm("게임 대기 방을 나가시겠습니까?")) return;
-    sockets[namespace].emitWithTime("exited", { my_player_id: myPlayerId });
-    try {
-      // const resData = await del(API_ENDPOINTS.PLAYERS(myPlayerId));
-      navigate("/game/list");
-      console.log("방 나가기 성공", resData);
-    } catch (error) {
-      console.log("방 나가기 요청 실패: ", error);
-    }
+    sockets[namespace].emitWithTime("exited", { my_player_id: myPlayerData.id });
+    navigate("/game/list");
+    console.log("방 나가기 성공");
   };
 
   return (
