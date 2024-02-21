9702fd2a1226ea4b7a123140afa6d2eb6a609730
yonaMac <yonaimyo@gmail.com>
2024-02-22 02:11:21 +0900

    FY-171/refactor: 퐁씬 페이지 필요없는 코드 삭제

--- src/pages/PongScenePage.js
+++ src/pages/PongScenePage.js
@@ -1,47 +1,8 @@
 import React, { useEffect } from "react";
 import PongScene from "../components/game/PongScene";
+import { useGame } from "../context/GameContext";
 
 const PongScenePage = () => {
-  const { roomData, subgameData } = useGame();
-  const roomNamespace = `/game/room/${roomData?.id}`;
-
-  useEffect(() => {
-    const matchNamespace = `${roomNamespace}/${subgameData.rank}/${subgameData.idx_in_rank}`;
-    setupEventListenersNamespace(matchNamespace, [
-      {
-        event: "update_time_left",
-        handler: null,
-      },
-      {
-        event: "ended",
-        handler: null,
-      },
-      {
-        event: "update_scores",
-        handler: null,
-      },
-      {
-        event: "update_track_ball",
-        handler: null,
-      },
-      {
-        event: "update_track_paddle",
-        handler: null,
-      },
-    ]);
-
-    return () => {
-      removeEventListenersNamespace(matchNamespace, [
-        "update_time_left",
-        "ended",
-        "update_scores",
-        "update_track_ball",
-        "update_track_paddle",
-      ]);
-      // disconnectNamespace(matchNamespace);
-    };
-  }, [matchNamespace, subgameData]);
-
   return <PongScene />;
 };
 
