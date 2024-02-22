e540e526fdf9531cdbd7ac06e0d93ede021d4156
yonaMac <yonaimyo@gmail.com>
2024-02-22 20:05:21 +0900

    fix: 대기방 페이지 로드시 방 존재 및 권한 여부 확인

--- src/pages/game/GameWaitingRoomPage.js
+++ src/pages/game/GameWaitingRoomPage.js
@@ -3,6 +3,8 @@ import WaitingRoomBox from "../../components/waiting_room/WaitingRoomBox";
 import { useParams, useNavigate } from "react-router-dom";
 import { useGame } from "../../context/GameContext";
 import LoadingPage from "../LoadingPage";
+import { get } from "../../utils/apiBase";
+import { API_ENDPOINTS } from "../../utils/apiEndpoints";
 
 const GameWaitingRoomPage = () => {
   const {
@@ -18,9 +20,27 @@ const GameWaitingRoomPage = () => {
   const navigate = useNavigate();
   const { room_id } = useParams();
 
-  // 마운트시에 room 소켓 연결 수립
+  // 마운트시에 해당 방의 참가자인지 확인하고 room 소켓 연결 수립
   useEffect(() => {
-    connectRoomSocket(room_id);
+    const checkIamInRoom = async (roomId) => {
+      try {
+        await get(API_ENDPOINTS.ROOM(roomId));
+        return true;
+      } catch (error) {
+        if (error.response.status === 404) alert("해당 방이 존재하지 않습니다.");
+        else if (error.response.status === 400) alert("해당 방의 참가자가 아닙니다.");
+        console.error(error);
+        navigate("/game/list");
+        return false;
+      }
+    };
+
+    (async () => {
+      const isInRoom = await checkIamInRoom(room_id);
+      if (isInRoom) {
+        connectRoomSocket(room_id);
+      }
+    })();
   }, [room_id]);
 
   // room 소켓의 연결이 수립되면 리스너를 세팅
