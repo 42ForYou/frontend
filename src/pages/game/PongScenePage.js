8da53ebbf1674484bb1f46e80205e407ae6cc106
yonaMac <yonaimyo@gmail.com>
2024-02-23 17:39:05 +0900

    FY-158/feat: PongScenePage 필요 데이터 없을시 로딩페이지 띄움

--- src/pages/game/PongScenePage.js
+++ src/pages/game/PongScenePage.js
@@ -1,8 +1,14 @@
-import React, { useEffect } from "react";
+import React from "react";
 import PongScene from "../../components/game/PongScene";
 import { useGame } from "../../context/GameContext";
+import LoadingPage from "../LoadingPage";
 
+// todo: 렌더에 필요한 데이터 추가
 const PongScenePage = () => {
+  const { configData, trackBall } = useGame();
+
+  if (!configData || !trackBall) return <LoadingPage />;
+
   return <PongScene />;
 };
 
