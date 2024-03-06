e22c712ef95130b8630443aadde98e69cdb161fc
yonaMac <yonaimyo@gmail.com>
2024-03-06 11:59:28 +0900

    FY-175/style: PlayerInModal 컴포넌트에 승패 유무 UI 추가

--- src/components/game/PlayerInModal.js
+++ src/components/game/PlayerInModal.js
@@ -1,11 +1,16 @@
 import React from "react";
 import Avatar from "../common/Avatar";
 
-const PlayerInModal = ({ nickname, avatar, isMine }) => {
+const WinOrLose = ({ isWinner }) => {
+  return <div style={{ fontWeight: "bold" }}>{isWinner ? "WINNER!" : "LOSER..."}</div>;
+};
+
+const PlayerInModal = ({ nickname, avatar, isMine, isWinner = undefined }) => {
   return (
     <div className="player-in-modal text-center">
+      {isWinner !== undefined && <WinOrLose isWinner={isWinner} />}
       <Avatar src={avatar} diameter={130} />
-      <p className="pt-2 mb-0" style={{ color: `${isMine ? "yellow" : ""}`, fontWeight: "bold" }}>
+      <p className="pt-2 mb-0" style={{ color: `${isMine ? "yellow" : ""}` }}>
         {nickname}
       </p>
     </div>
