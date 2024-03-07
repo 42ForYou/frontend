51c6c46bf25221baf423bea659d28f0c7827e6d6
yonaMac <yonaimyo@gmail.com>
2024-03-07 21:45:36 +0900

    FY-206/fix: 딜레이 시간 원래대로 변경

--- src/pages/game/GamePlayPage.js
+++ src/pages/game/GamePlayPage.js
@@ -35,9 +35,12 @@ const GamePlayPage = () => {
   useEffect(() => {
     if (!bracketData) return;
 
+    if (rankOngoing === bracketData.rank_ongoing) return; // 현재 진행중인 "강"이 같으면 무시
     const newRankOngoing = bracketData.rank_ongoing;
-    if (rankOngoing === newRankOngoing) return; // 현재 진행중인 "강"이 같으면 무시
     const myFinalSubgameAndRank = getMyFinalSubgameAndRank(bracketData.subgames);
+
+    console.log("새로운 대진표가 왔습니다.");
+    console.log(newRankOngoing, myFinalSubgameAndRank);
 
     setShowBracket(true);
     setShowSubgameResultModal(false);
@@ -50,7 +53,6 @@ const GamePlayPage = () => {
       disconnectRoomSocket();
     } else {
       // 다음 "강"으로 넘어감
-      console.log("다음 강으로 넘어갑니다.");
       connectNextSubgameSocket(bracketData);
       setRankOngoing(newRankOngoing);
     }
@@ -65,23 +67,24 @@ const GamePlayPage = () => {
 
     console.log("서브게임 시작 시간: ", startTime);
     console.log("현재 시간: ", now);
-    console.log("서브게임 시뮬레이션까지 대기시간: ", delay / 2);
+    console.log("서브게임 시뮬레이션까지 대기시간: ", delay);
 
     const updateRemainingTime = () => {
       const currentNow = new Date().getTime();
-      const timeLeft = Math.max(startTime - currentNow, 0) / 1000 / 2;
+      const timeLeft = Math.max(startTime - currentNow, 0) / 1000;
       setRemainingTime(Math.ceil(timeLeft));
 
       if (timeLeft > 0) {
         setTimeout(updateRemainingTime, 1000); // 1초 후에 다시 업데이트
       } else {
+        console.log("서브게임 시뮬레이션까지 대기시간이 끝났습니다.");
         setShowSubgameBracketModal(false);
         setShowBracket(false);
       }
     };
 
     if (delay > 0) {
-      setRemainingTime(Math.ceil(delay / 1000 / 2)); // 처음에 남은 시간을 설정
+      setRemainingTime(Math.ceil(delay / 1000)); // 처음에 남은 시간을 설정
       setShowSubgameBracketModal(true);
       setTimeout(updateRemainingTime, 1000); // 1초 후에 남은 시간 업데이트 시작
     } else {
