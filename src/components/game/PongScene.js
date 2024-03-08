038568bbde3237caf2a75a839feb1fe7cb9cfec0
yonaMac <yonaimyo@gmail.com>
2024-03-08 16:33:48 +0900

    style: 퐁 씬 카메라 설정 변경

--- src/components/game/PongScene.js
+++ src/components/game/PongScene.js
@@ -76,7 +76,7 @@ const PongScene = () => {
 
     // 카메라 생성
     const newCamera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-    newCamera.position.set(0, -500, 700);
+    newCamera.position.set(0, -100, 700);
     newCamera.up.set(0, 0, 1); // 카메라의 업벡터를 z축으로 설정
     newCamera.lookAt(new THREE.Vector3(0, 0, 0));
     setCamera(newCamera);
