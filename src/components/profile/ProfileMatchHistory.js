0eb12abdf7adef08fe2eada86d248199d809bf73
yonaMac <yonaimyo@gmail.com>
2024-03-09 17:04:02 +0900

    FY-207/feat: ProfileMatchHistory 컴포넌트 구현

---
+++ src/components/profile/ProfileMatchHistory.js
@@ -0,0 +1,60 @@
+import React from "react";
+
+// todo: 서브게임 리스트에서 일부만 잘라 띄우거나 클릭하면 모달로 띄우기
+
+const MatchHistoryTable = ({ matchHistoryData }) => {
+  const formatDate = (timestamp) => {
+    const date = new Date(timestamp * 1000);
+    return date.toLocaleString();
+  };
+
+  const getMode = (game) => (game.is_tournament ? "토너먼트" : "1vs1");
+
+  const getRank = (rank) => {
+    if (rank === -1) return "우승";
+    if (rank === 0) return "결승전";
+    return `${2 ** (rank + 1)}강`;
+  };
+
+  const getGameOption = (game) => `목표: ${game.game_point}점, 제한시간 ${game.time_limit}초`;
+
+  return (
+    <table className="table">
+      <thead>
+        <tr>
+          <th>일시</th>
+          <th>모드</th>
+          <th>라운드</th>
+          <th>게임 옵션</th>
+          <th>최종득점</th>
+          <th>승리여부</th>
+        </tr>
+      </thead>
+      <tbody>
+        {matchHistoryData.map((gameData) =>
+          gameData.subgames.map((subgame, index) => (
+            <tr key={`${gameData.game.game_id}_${index}`}>
+              <td>{formatDate(subgame.t_start)}</td>
+              <td>{getMode(gameData.game)}</td>
+              <td>{getRank(subgame.rank)}</td>
+              <td>{getGameOption(gameData.game)}</td>
+              <td>{`${subgame.point_a} : ${subgame.point_b}`}</td>
+              <td>{subgame.game_player_won ? "승리" : "패배"}</td>
+            </tr>
+          ))
+        )}
+      </tbody>
+    </table>
+  );
+};
+
+const ProfileMatchHistory = ({ matchHistoryData }) => {
+  return (
+    <div className="mt-4">
+      <h2 className="mb-4">Match History</h2>
+      <MatchHistoryTable matchHistoryData={matchHistoryData} />
+    </div>
+  );
+};
+
+export default ProfileMatchHistory;
