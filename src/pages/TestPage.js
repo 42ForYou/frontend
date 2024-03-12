d313642d1e48f567ea9dfc00e58c86eb10844fd8
yonaMac <yonaimyo@gmail.com>
2024-03-12 14:25:59 +0900

    FY-210/test: 테스트용 더미데이터 업데이트

--- src/pages/TestPage.js
+++ src/pages/TestPage.js
@@ -7,7 +7,7 @@ const subgame = {
   // 4강전 2경기
   player_a: {
     intra_id: "yeonhkim3",
-    nickname: "kimyeonhababo",
+    nickname: "yeonhkim",
     avatar: "./pengdori.jpg",
   },
   player_b: {
@@ -19,7 +19,7 @@ const subgame = {
   winner: "A", // 경기가 끝나면 정해짐
 };
 
-const bracketData = {
+const middleBracketData = {
   n_ranks: 2,
   rank_ongoing: 1,
   subgames: [
@@ -72,13 +72,66 @@ const bracketData = {
   ],
 };
 
+const winBracketData = {
+  n_ranks: 2,
+  rank_ongoing: -1,
+  subgames: [
+    [
+      {
+        player_a: {
+          intra_id: "yeonhkim",
+          nickname: "yeonhkimdd",
+          avatar: "./pengdori.jpg",
+        },
+        player_b: {
+          intra_id: "플레이어 2",
+          nickname: "baboyeonhkim",
+          avatar: "./pengdori.jpg",
+        },
+        winner: "A", // 경기가 끝나면 정해짐
+      },
+    ],
+    [
+      {
+        // 4강전 1경기
+        player_a: {
+          intra_id: "플레이어 1",
+          nickname: "yeonhkimbabo",
+          avatar: "./pengdori.jpg",
+        },
+        player_b: {
+          intra_id: "yeonhkim2",
+          nickname: "baboyeonhkim",
+          avatar: "./pengdori.jpg",
+        },
+        winner: null, // 경기가 끝나면 정해짐
+      },
+      {
+        // 4강전 2경기
+        player_a: {
+          intra_id: "yeonhkim3",
+          nickname: "kimyeonhababo",
+          avatar: "./pengdori.jpg",
+        },
+        player_b: {
+          intra_id: "yeonhkim4",
+          nickname: "baboyona",
+          avatar: "./pengdori.jpg",
+        },
+        winner: null, // 경기가 끝나면 정해짐
+      },
+    ],
+  ],
+};
+
 const TestPage = () => {
   return (
     <div>
       <h1>TestPage</h1>
-      <TournamentResultModal bracketData={bracketData} />
+      {/* <TournamentResultModal bracketData={middleBracketData} /> */}
+      {/* <TournamentResultModal bracketData={winBracketData} /> */}
       {/* <SubgameBracketModal playerA={subgame.player_a} playerB={subgame.player_b} remainingTime={42} /> */}
-      {/* <SubgameResultModal playerA={subgame.player_a} playerB={subgame.player_b} winner={subgame.winner} /> */}
+      <SubgameResultModal playerA={subgame.player_a} playerB={subgame.player_b} winner={subgame.winner} />
     </div>
   );
 };
