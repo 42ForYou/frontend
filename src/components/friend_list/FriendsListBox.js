fd106f0fbe201aa5a711924a3aaf0f90d154dd4b
yonaMac <yonaimyo@gmail.com>
2024-03-16 15:16:14 +0900

    style: 친구 리스트 한 줄당 아이템 개수 유동적으로 조정

--- src/components/friend_list/FriendsListBox.js
+++ src/components/friend_list/FriendsListBox.js
@@ -3,8 +3,10 @@ import React from "react";
 import FriendItem from "./FriendItem";
 import ListBox from "../list/ListBox";
 import { API_ENDPOINTS } from "../../utils/apiEndpoints";
+import { useLayout } from "../../context/LayoutContext";
 
 const FriendsListBox = () => {
+  const { isWide } = useLayout();
   const filterTypes = [
     { value: "friend", label: "나의 친구" },
     { value: "pending", label: "내게 온 친구요청" },
@@ -18,7 +20,7 @@ const FriendsListBox = () => {
         filterTypes={filterTypes}
         emptyMsg={"친구 데이터가 없습니다."}
         itemsPerPage={6}
-        itemsPerRow={3}
+        itemsPerRow={isWide ? 3 : 2}
       />
     </>
   );
