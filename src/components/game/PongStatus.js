1769647d14acdc3f7b9411b955fbfbad94090d86
yonaMac <yonaimyo@gmail.com>
2024-03-21 15:38:15 +0900

    style: PongStatus 스타일링

--- src/components/game/PongStatus.js
+++ src/components/game/PongStatus.js
@@ -6,7 +6,7 @@ const PongPlayer = ({ player }) => {
   return (
     <div className="d-flex-col justify-content-center align-items-center">
       <Avatar src={player.avatar} alt={`${player.nickname}'s avatar`} diameter={80} />
-      <h5 className="mt-1">{player.nickname}</h5>
+      <h5 className="pt-1">{player.nickname}</h5>
     </div>
   );
 };
@@ -18,7 +18,7 @@ const PongStatus = () => {
     if (sudden_death) {
       return <span className="SuddenDeath">Sudden Death</span>;
     } else if (time_left > 0) {
-      return <span>Time: ${time_left}</span>;
+      return <span>Time: {time_left}</span>;
     } else {
       return <span>Game Over!</span>;
     }
@@ -26,15 +26,15 @@ const PongStatus = () => {
 
   return (
     <div className="PongStatus">
-      <div style={{ position: "absolute", top: "2%", left: "10%", transform: "translate(-50%, 0%)", width: "100%" }}>
+      <div style={{ position: "absolute", top: "2%", left: "10%", transform: "translate(-50%, 0%)" }}>
         <PongPlayer player={subgameStatus.player_a} />
       </div>
-      <div style={{ position: "absolute", top: "2%", left: "90%", transform: "translate(-50%, 0%)", width: "100%" }}>
+      <div style={{ position: "absolute", top: "2%", left: "90%", transform: "translate(-50%, 0%)" }}>
         <PongPlayer player={subgameStatus.player_b} />
       </div>
       <span
         className="text-center"
-        style={{ position: "absolute", top: "2%", left: "50%", transform: "translate(-50%, 0%)", width: "100%" }}>
+        style={{ position: "absolute", top: "2%", left: "50%", transform: "translate(-50%, 0%)" }}>
         <h2>
           {subgameStatus.score_a} : {subgameStatus.score_b}
         </h2>
